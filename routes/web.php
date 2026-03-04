8d87c20a9bea53df017e3ba9d35ad5be4cc215ca
Joseph Atem Deng Aruei <atemhatingaa@gmail.com>
2026-03-04 18:12:16 +0700

    Improve Kategori CRUD functionality and update controller logic

--- routes/web.php
+++ routes/web.php
@@ -29,6 +29,16 @@
 // Route::get('/sales', [SalesController::class, 'index']);R
 
 
+// use App\Http\Controllers\LevelController;
+// use Illuminate\Support\Facades\Route;
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// Route::get('/level', [LevelController::class, 'index']);
+
+use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\LevelController;
 use Illuminate\Support\Facades\Route;
 
@@ -37,3 +47,4 @@ Route::get('/', function () {
 });
 
 Route::get('/level', [LevelController::class, 'index']);
+Route::get('/kategori', [KategoriController::class, 'index']);
